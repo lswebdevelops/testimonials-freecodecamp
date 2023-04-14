a992a6f15ddde87f38a291a48c26a51c46d88f42
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-13 21:03:26 -0300

    adding classes to app and Testimonials

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
 import './App.css';
-import { Testimonials } from './components/Testimonials';
+import  Testimonials from './components/Testimonials';
+import React from 'react';
 
-
-<h1>test</h1>
-
-function App() {
-  return (
-    <div className="App">
-      <div className='main-container'>
-      <h1> Here is what our alumni say about freeCodeCamp:</h1>
-      <Testimonials
-      name='Shawn Wang'
-      image='shawn'
-      firstName='Shawn'
-      profession='Software Engineer'
-      company='Amazon'
-      country='Singapore'
-      testimonials="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
-
-      />
-      <Testimonials
-      name='Sarah Chima'
-      image='sarah'
-      firstName='Sarah'
-      profession='Software Engineer'
-      company='ChatDesk'
-      country='Nigeria'
-      testimonials="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
-      
-      />
-      <Testimonials
-      name='Emma Bostian'
-      image='emma'
-      firstName='Emma'
-      profession='Software Engineer'
-      company='Spotify'
-      country='Sweden'
-      testimonials="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
-      
-      />
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <div className='main-container'>
+        <h1> Here is what our alumni say about freeCodeCamp:</h1>
+        <Testimonials
+        name='Shawn Wang'
+        image='shawn'
+        firstName='Shawn'
+        profession='Software Engineer'
+        company='Amazon'
+        country='Singapore'
+        testimonials="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+  
+        />
+        <Testimonials
+        name='Sarah Chima'
+        image='sarah'
+        firstName='Sarah'
+        profession='Software Engineer'
+        company='ChatDesk'
+        country='Nigeria'
+        testimonials="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        
+        />
+        <Testimonials
+        name='Emma Bostian'
+        image='emma'
+        firstName='Emma'
+        profession='Software Engineer'
+        company='Spotify'
+        country='Sweden'
+        testimonials="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        
+        />
+        </div>
       </div>
-    </div>
-  );
+    );
+  
+  }
 }
+
 
 export default App;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,20 +1,24 @@
 import React from "react";
+
 import '../style/Testimonials.css'
 
-
-export function Testimonials(props){
-    return(
-        <div className="testimonial-container">
-            <img className="image-container" 
-            src={require(`../images/testimonials-${props.image}.png`)}
-            alt={`A photo of ${props.firstName}`}/>
-            <div className="para-container">
-                <p className="name-container">{props.name} in {props.country}</p>
-                <p className="profession-container">Software Engineer at {props.company}</p>
-                <p className="about-container">"{props.testimonials}"</p>
+class Testimonials extends React.Component{
+    render(){
+        return(
+            <div className="testimonial-container">
+                <img className="image-container" 
+                src={require(`../images/testimonials-${this.props.image}.png`)}
+                alt={`A photo of ${this.props.firstName}`}/>
+                <div className="para-container">
+                    <p className="name-container">{this.props.name} in {this.props.country}</p>
+                    <p className="profession-container">Software Engineer at {this.props.company}</p>
+                    <p className="about-container">"{this.props.testimonials}"</p>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
+
+export default Testimonials;
 
 
